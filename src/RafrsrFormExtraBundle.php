c4cf45ae4632e38d90b0e379891d368a465d4fd9
Rafael SR <raf.rsr@gmail.com>
2016-03-09 17:24:44 -0500

    draft empty bundle

---
+++ src/RafrsrFormExtraBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * LICENSE: This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * @copyright 2016 Copyright(c) - All rights reserved.
+ */
+
+namespace Rafrsr\FormExtraBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RafrsrFormExtraBundle
+ */
+class RafrsrFormExtraBundle extends Bundle
+{
+}
