f5ba415f0ae16d779d7a8d9ec4a858d35c2a992d
Rafael SR <raf.rsr@gmail.com>
2016-05-18 14:56:03 -0400

    html formatted options

---
+++ src/Form/Transformer/TagsTransfromer.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * LICENSE: This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * @copyright 2016 Copyright(c) - All rights reserved.
+ */
+
+namespace Rafrsr\FormExtraBundle\Form\Transformer;
+
+/**
+ * Class TagsTransfromer
+ */
+class TagsTransfromer
+{
+
+}
