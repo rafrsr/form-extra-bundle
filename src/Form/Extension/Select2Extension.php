d0a0c3531870e3da2ab0cf9bc5b16509522e0cc0
Rafael SR <raf.rsr@gmail.com>
2016-05-18 20:13:03 -0400

    html formatted options

--- src/Form/Extension/Select2Extension.php
+++ src/Form/Extension/Select2Extension.php
@@ -9,7 +9,9 @@
 
 namespace Rafrsr\FormExtraBundle\Form\Extension;
 
+use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
@@ -26,6 +28,11 @@ class Select2Extension extends AbstractTypeExtension
      */
     private $templating;
 
+    /**
+     * @var RegistryInterface
+     */
+    private $doctrine;
+
     private $select2DefaultOptions
         = [
             'minimumResultsForSearch' => 20,
@@ -34,9 +41,10 @@ class Select2Extension extends AbstractTypeExtension
     /**
      * @param EngineInterface $templating
      */
-    public function __construct(EngineInterface $templating)
+    public function __construct(EngineInterface $templating, RegistryInterface $doctrine = null)
     {
         $this->templating = $templating;
+        $this->doctrine = $doctrine;
     }
 
     /**
@@ -44,6 +52,29 @@ class Select2Extension extends AbstractTypeExtension
      */
     public function finishView(FormView $view, FormInterface $form, array $options)
     {
+        /** @var ChoiceView $choice */
+        foreach ($view->vars['choices'] as $choice) {
+            if ($options['select2_template_result']) {
+
+                $object = $choice->value;
+                if ($this->doctrine && $options['class']) {
+                    $object = $this->doctrine->getRepository($options['class'])->find($object);
+                }
+                if (is_string($options['select2_template_result'])) {
+                    $choice->attr['data-template-result'] = $this->templating->render(
+                        $options['select2_template_result'],
+                        [
+                            'choice' => $choice,
+                            'object' => $object
+                        ]
+                    );
+                } else {
+                    $choice->attr['data-template-result']
+                        = call_user_func_array($options['select2_template_result'], [$choice, $object]);
+                }
+            }
+        }
+
         if ($options['select2'] === true) {
             $options['select2_options'] = array_merge($this->select2DefaultOptions, $options['select2_options']);
             $view->vars['select2_options'] = json_encode($options['select2_options']);
@@ -59,10 +90,14 @@ class Select2Extension extends AbstractTypeExtension
             [
                 'select2' => true,
                 'select2_options' => [],
+                'select2_template_result' => null,
+                'select2_template_selection' => null,
             ]
         );
         $resolver->setAllowedTypes('select2', ['boolean']);
         $resolver->setAllowedTypes('select2_options', ['null', 'array']);
+        $resolver->setAllowedTypes('select2_template_result', ['null', 'string', 'callable']);
+        $resolver->setAllowedTypes('select2_template_selection', ['null', 'string', 'callable']);
     }
 
     /**
